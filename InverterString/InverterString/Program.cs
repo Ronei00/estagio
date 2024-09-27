3c9a1e6cfa523a7458292675c28c33b229a751b8
Ronei <128158454+Ronei00@users.noreply.github.com>
2024-09-27 10:57:09 -0300

    Add files via upload

---
+++ InverterString/InverterString/Program.cs
@@ -0,0 +1,14 @@
+﻿string text2 = "";
+Console.WriteLine("escreva uma frase para colocar invertida");
+string text = Console.ReadLine();
+
+char[] array = text.ToCharArray();
+int j = array.Length - 1;
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+    text2 += array[j];
+    j--;
+}
+Console.WriteLine(text2);
