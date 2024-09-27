85364fed89b13e41bdb26238bf5be3ad091a6ff9
Ronei <128158454+Ronei00@users.noreply.github.com>
2024-09-27 10:53:06 -0300

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿int fibo = 0;
+int temp;
+Console.WriteLine("Por favor digite um número!!");
+
+string valordigitado = Console.ReadLine();
+
+while (valordigitado == "")
+{
+    Console.WriteLine("Por favor digite um número!!");
+
+    valordigitado = Console.ReadLine();
+}
+
+if (valordigitado != "") 
+{
+    int digitado = int.Parse(valordigitado);
+
+    for (int i = 1; fibo < digitado;)
+    {
+        temp = fibo;
+        fibo = i + fibo;
+        i = temp;
+        if (fibo == digitado)
+        {
+        Console.WriteLine("Valor digitado pertence a fibonacci");
+        }
+    }
+
+    if(fibo > digitado)
+    {
+    Console.WriteLine("Valor digitado não pertence a fibonacci");
+    }
+}
